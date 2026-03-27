6b63c2db883e686760d5a4cd35fcd25b34a6728d
DylanT <nalyd1212@gmail.com>
2026-03-27 17:58:35 -0400

    added comments to typetraits.h

---
+++ Sandbox/functiontests.h
@@ -0,0 +1,13 @@
+// The main purpose of this file is to test specific aspects of the simulation
+
+#include <iostream>
+#include <numbers>
+#include <cmath>
+#include <iomanip> // Optional: for setting precision
+using namespace std;
+#include <cstdlib> // for rand() and srand()
+
+
+double squared(double x) {
+    return x * x;
+}
