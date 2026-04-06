cd500114b12a626950856c47070ed4f49a15606b
DylanT <nalyd1212@gmail.com>
2026-04-06 16:36:05 -0400

    Finished Circle.h analysis

--- Circle.h
+++ Circle.h
@@ -9,7 +9,9 @@
 #include "Utilities.h"
 
 
-
+/* Class that creates ellipses based on position, a = horizontal radius, b = vertical radius, 
+radius = collision interaction radius, and value = voltage. This class contains positions, velocity,
+accelerations, and other core parameters for collision and transit calcuations.*/
 struct Circle {
     // a is semi-major (horizontal) axis, b is semi-minor (vertical) axis
     Circle(const V2<int>& position, int a, int b, int radius = 0, Value value = 0) : position{ position }, a{ a }, b{ b }, radius{ radius }, value{ value }, id{ CircleCount() } {}
@@ -23,7 +25,7 @@ struct Circle {
     int radius{ 0 };
     Value value;
     int id;
-
+    // Redefine == and != operators to check if two circles are the same
     bool operator==(const Circle& other) const {
         return id == other.id;
     }
@@ -31,14 +33,28 @@ struct Circle {
     bool operator!=(const Circle& other) const {
         return !(*this == other);
     }
-
+    // Creates a unique ID for each circle
     static int CircleCount() {
         static int id{ 0 };
         return id++;
     }
-
+    // Creates a V2 vector for the points on the perimeter of the circle using a simple rounding method
     std::vector<V2<int>> GetPerimeterPoints() const {
         std::vector<V2<int>> perimeter_points;
+
+        int num_points = 1000;
+        // Loop to calculate points at different angles
+        for (int i = 0; i < num_points; ++i) {
+            double theta = 2 * std::numbers::pi * i / num_points;  // Angle between 0 and 2pi
+            double x = a * cos(theta) + position.x;  // x coordinate
+            double y = b * sin(theta) + position.y;  // y coordinate
+            V2<int> point = Round(V2<double>{ x, y });
+            if (std::find(perimeter_points.begin(), perimeter_points.end(), point) == perimeter_points.end()) {
+                perimeter_points.push_back(point);  // Add point to the vector
+            }
+        }
+
+        // These commented out sections are alternative methods for getting the points, can be deleted later if you want
         /*
         double arc = 0.5;
         double phi = 0;
@@ -53,19 +69,6 @@ struct Circle {
                 phi += arc / radius;
             }
         }*/
-
-        int num_points = 1000;
-        // Loop to calculate points at different angles
-        for (int i = 0; i < num_points; ++i) {
-            double theta = 2 * std::numbers::pi * i / num_points;  // Angle between 0 and 2pi
-            double x = a * cos(theta) + position.x;  // x coordinate
-            double y = b * sin(theta) + position.y;  // y coordinate
-            V2<int> point = Round(V2<double>{ x, y });
-            if (std::find(perimeter_points.begin(), perimeter_points.end(), point) == perimeter_points.end()) {
-                perimeter_points.push_back(point);  // Add point to the vector
-            }
-        }
-
 
         /*
         V2<int> point{ 0,radius };
@@ -97,16 +100,16 @@ struct Circle {
         */
         return perimeter_points;
     }
-
-private:
-    void AddPerimeterPoints(std::unordered_set<V2<int>>& v, V2<int> point ) const {
-        v.emplace(position.x + point.x, position.y + point.y);
-        v.emplace(position.x - point.x, position.y + point.y);
-        v.emplace(position.x + point.x, position.y - point.y);
-        v.emplace(position.x - point.x, position.y - point.y);
-        v.emplace(position.x - point.y, position.y + point.x);
-        v.emplace(position.x + point.y, position.y - point.x);
-        v.emplace(position.x - point.y, position.y - point.x);
-        v.emplace(position.x + point.y, position.y + point.x);
-    }
+// Older, now defunct function that can be used to generate perimeter points based on known boundry conditions
+// private:
+//     void AddPerimeterPoints(std::unordered_set<V2<int>>& v, V2<int> point ) const {
+//         v.emplace(position.x + point.x, position.y + point.y);
+//         v.emplace(position.x - point.x, position.y + point.y);
+//         v.emplace(position.x + point.x, position.y - point.y);
+//         v.emplace(position.x - point.x, position.y - point.y);
+//         v.emplace(position.x - point.y, position.y + point.x);
+//         v.emplace(position.x + point.y, position.y - point.x);
+//         v.emplace(position.x - point.y, position.y - point.x);
+//         v.emplace(position.x + point.y, position.y + point.x);
+//     }
 };
